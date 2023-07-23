931a80704472e576d9242727c26ede08a7ae4249
Sara BEGACHE <sara.begache@gmail.com>
2023-07-23 11:40:22 +0100

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// 1. Initialize the `employee` object with name and streetAddress keys
+const employee = {
+    name: "Sara BEGACHE",
+    streetAddress: "Algiers Algeria",
+  };
+  
+  // 2. Create the `updateEmployeeWithKeyAndValue` function
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a new object with the updated key-value pair using the spread operator
+    return { ...employee, [key]: value };
+  }
+  
+  // 3. Create the `destructivelyUpdateEmployeeWithKeyAndValue` function
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update the original object with the new key-value pair
+    employee[key] = value;
+    return employee;
+  }
+  
+  // 4. Create the `deleteFromEmployeeByKey` function
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object without the specified key using the spread operator
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // 5. Create the `destructivelyDeleteFromEmployeeByKey` function
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key from the original object
+    delete employee[key];
+    return employee;
+  }
+  
+  
